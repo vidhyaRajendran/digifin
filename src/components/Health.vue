293a6d98ff41f35d745878d6c1c886b385c9781a
iVidhyaRajendran <vidhya@constient.com>
2020-07-22 12:57:57 +0530

    json formed

--- src/components/Health.vue
+++ src/components/Health.vue
@@ -27,13 +27,46 @@
       <v-flex xs10>
         <div v-for="list in plan_list" :key="list">
           <v-card style="margin-left: 30px; height: 200px">
-            <v-checkbox style="margin-left:10px" v-model="list.checkboxValue" :label="list.plan_name"></v-checkbox>
-            <v-btn class="white--text" style="margin-left:750px; background-color:#fc5350; font-color:white">Buy for Rs.{{list.price}}
-            </v-btn>
-            <br/>
-            <span style="margin-left:800px; font-size:10px">only Rs.{{list.per_day_price}}/ day </span>
+            <v-checkbox
+              style="margin-left:10px"
+              v-model="list.checkboxValue"
+              :label="list.plan_name"
+            ></v-checkbox>
+            <v-container fluid>
+              <v-data-iterator :items="list" hide-default-footer>
+                <template v-slot:default="props">
+                  <v-row>
+                    <v-col v-for="item in props" :key="item" cols="12" sm="6" md="4" lg="3">
+                      <v-card>
+                        <v-card-title class="subheading font-weight-bold">Top 4 Benefits</v-card-title>
+
+                        <v-divider></v-divider>
+
+                        <v-list dense>
+                          <v-list-item>
+                            <v-list-item-content>1. Room Rent</v-list-item-content>
+                            <v-list-item-content class="align-end">2. No Claim Bonus</v-list-item-content>
+                          </v-list-item>
+
+                          <v-list-item>
+                            <v-list-item-content>3. Co payment</v-list-item-content>
+                            <v-list-item-content class="align-end">4. PEO Waiting Period</v-list-item-content>
+                          </v-list-item>
+                        </v-list>
+                      </v-card>
+                    </v-col>
+                  </v-row>
+                </template>
+              </v-data-iterator>
+            </v-container>
+            <v-btn
+              class="white--text"
+              style="margin-left:750px; background-color:#fc5350; font-color:white"
+            >Buy for Rs.{{list.price}}</v-btn>
+            <br />
+            <span style="margin-left:800px; font-size:10px">only Rs.{{list.per_day_price}}/ day</span>
             <span style="margin-left:360px;" class="hq-policy">Policy Boucher</span>
-            <span style="margin-left: -430px;" class="hq-policy"> Plan Detail</span>
+            <span style="margin-left: -430px;" class="hq-policy">Plan Detail</span>
           </v-card>
         </div>
       </v-flex>
@@ -42,224 +75,16 @@
 </template>
 
 <script>
+import profile from "../data/profile";
+import preferences from "../data/preferences";
+import plan_list from "../data/plan_list";
 export default {
   name: "Health",
   data() {
     return {
-      my_profile: [
-        {
-          name: "Adults",
-          value: 1
-        },
-        {
-          name: "Children",
-          value: 0
-        },
-        {
-          name: "Age",
-          value: "25 Years"
-        },
-        {
-          name: "Sum Insured",
-          value: "5 lacs"
-        },
-        {
-          name: "Gender",
-          value: "Male"
-        },
-        {
-          name: "city",
-          value: "New Delhi"
-        }
-      ],
-      preferences: [
-        {
-          name: "Room Rent"
-        },
-        {
-          name: "No Claim Bonus"
-        },
-        {
-          name: "Co Payment"
-        },
-        {
-          name: "S I Restoration"
-        },
-        {
-          name: "PED Waiting Period"
-        },
-        {
-          name: "Day Care Procedures"
-        },
-        {
-          name: "Health Check Up"
-        },
-        {
-          name: "Wellness"
-        },
-        {
-          name: "AYUSH"
-        }
-      ],
-      plan_list: [
-        {
-          plan_name: "Health Companion",
-          checkboxValue: "false",
-          plan_image: "1.png",
-          score: 86.9,
-          price: "7138",
-          per_day_price: "19.56",
-          benefits: [
-            {
-              name: "Room Rent"
-            },
-            {
-              name: "No Claim Bonus"
-            },
-            {
-              name: "Co Payment"
-            },
-            {
-              name: "SI Restoration"
-            }
-          ],
-          top_benfifts: {
-            sum_assured: "5 Lacs",
-            premium: "7138",
-            room_rent: "No sub limits except for suite or above room category",
-            pre_existing_disease:
-              "Covered (after 3 years of continuous coverage for SI 5 lacs and above, after 4 years of continuous coverage for SI upto 4 lacs). For Family First Plan, 3 years waiting period is applicable",
-            waiting_period:
-              "Any diseases contracted during the first 30 days of commencement of the policy are not covered except for accidental cases",
-            co_pay_required: "Not Applicable",
-            ncb: "Available up to 100% of SI",
-            day_care_procedure: "All day care treatments covered",
-            pre_hosiptial_cover: "30 days",
-            post_hosiptial_cover: "60 days",
-            daily_cash_benfit: "Available as an add-on benefit",
-            home_hosipital: "Covered upto 100% of SI"
-          },
-          other_benifts: {
-            restore_benefit: true,
-            pregancy_covered: false,
-            health_checkup: true,
-            critical_illness_benefit: false,
-            regular_medical_expenses: false,
-            ambulance_service_covered: true,
-            lifelong_renewability: true,
-            organ_donor_coverage: true,
-            eye_cover: false,
-            dental_cover: false,
-            homeopathy: true,
-            recovery_benefit: false
-          }
-        },
-        {
-          plan_name: "Complete Health Care Insurance",
-          checkboxValue: "false",
-          plan_image: "2.png",
-          score: 86.6,
-          price: "6393",
-          per_day_price: "17.52",
-          benefits: [
-            {
-              name: "Room Rent"
-            },
-            {
-              name: "Co Payment"
-            },
-            {
-              name: "PED Waiting Period"
-            },
-            {
-              name: "SI Restoration"
-            }
-          ],
-          top_benfifts: {
-            sum_assured: "5 Lacs",
-            premium: "6393",
-            room_rent: "No sub limits on room rent",
-            pre_existing_disease:
-              "Covered (post 3 years of continued coverage)",
-            waiting_period:
-              "No medical treatments are covered within30 days from policy inception except for accidental emergencies",
-            co_pay_required: "Not Applicable",
-            ncb: "Available up to 50% of SI",
-            day_care_procedure: "141 Day Care Procedures covered",
-            pre_hosiptial_cover: "30 days",
-            post_hosiptial_cover: "60 days",
-            daily_cash_benfit: "Available as an add-on benefit",
-            home_hosipital: "Covered upto 20% of SI"
-          },
-          other_benifts: {
-            restore_benefit: true,
-            pregancy_covered: true,
-            health_checkup: true,
-            critical_illness_benefit: true,
-            regular_medical_expenses: true,
-            ambulance_service_covered: true,
-            lifelong_renewability: true,
-            organ_donor_coverage: true,
-            eye_cover: true,
-            dental_cover: true,
-            homeopathy: true,
-            recovery_benefit: true
-          }
-        },
-        {
-          plan_name: "Care Plus No Claim Bonus Super",
-          checkboxValue: "false",
-          plan_image: "3.png",
-          score: 82.5,
-          price: "7283",
-          per_day_price: "19.95",
-          benefits: [
-            {
-              name: "Room Rent"
-            },
-            {
-              name: "Co Payment"
-            },
-            {
-              name: "PED Waiting Period"
-            },
-            {
-              name: "No Claim Bonus"
-            }
-          ],
-          top_benfifts: {
-            sum_assured: "5 Lacs",
-            premium: "7283",
-            room_rent: "Single Private Room",
-            pre_existing_disease:
-              "Covered (post 4 years of continued coverage)",
-            waiting_period:
-              "Any diseases contracted during first 30 days of the policy start date except those arising out of accidents",
-            co_pay_required:
-              "20% of the Claim Amount (if insured is more than  61 years of age)",
-            ncb: "Available up to 150% of SI",
-            day_care_procedure: ">500 Day Care Procedures covered",
-            pre_hosiptial_cover: "30 days",
-            post_hosiptial_cover: "60 days",
-            daily_cash_benfit: "Rs 500 per day upto 5 days for SI upto 4 L",
-            home_hosipital: "Covered upto 10% of SI"
-          },
-          other_benifts: {
-            restore_benefit: true,
-            pregancy_covered: true,
-            health_checkup: true,
-            critical_illness_benefit: false,
-            regular_medical_expenses: false,
-            ambulance_service_covered: true,
-            lifelong_renewability: true,
-            organ_donor_coverage: true,
-            eye_cover: false,
-            dental_cover: false,
-            homeopathy: true,
-            recovery_benefit: false
-          }
-        }
-      ]
+      my_profile: profile.data,
+      preferences: preferences.data,
+      plan_list: plan_list.data
     };
   }
 };
@@ -267,13 +92,13 @@ export default {
 
 <style lang="scss">
 .hq-policy {
-    font-size: 12px;
-    color: #59ccc2;
-    text-decoration: underline;
-    width: 100%;
-    float: left;
-    text-align: center;
-    font-style: normal;
-    font-weight: 600;
+  font-size: 12px;
+  color: #59ccc2;
+  text-decoration: underline;
+  width: 100%;
+  float: left;
+  text-align: center;
+  font-style: normal;
+  font-weight: 600;
 }
 </style>
