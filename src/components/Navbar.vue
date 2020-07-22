f58e320cadebfd305556a7deebb1b37bc4494863
iVidhyaRajendran <vidhya@constient.com>
2020-07-22 18:14:20 +0530

    logo click added

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,25 +1,11 @@
 <template>
   <v-card color="grey lighten-4" flat tile>
     <v-toolbar dark>
-      <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
       <v-container>
         <v-toolbar-title>
           <img @click="changePath()" :src="digifin" alt="logo" />
         </v-toolbar-title>
-
         <v-spacer></v-spacer>
-
-        <!-- <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-        </v-btn>-->
       </v-container>
     </v-toolbar>
   </v-card>
